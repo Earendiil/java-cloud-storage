fe9872e64edbe7b390201f350b1bc57e21d3ce92
teo <teo.th@hotmail.gr>
2025-06-16 20:14:13 +0200

    docker-compose settings, endpoint permission added

--- CloudStorage/src/main/java/com/storage/security/SecurityConfig.java
+++ CloudStorage/src/main/java/com/storage/security/SecurityConfig.java
@@ -58,7 +58,8 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5173", "http://localhost"));
+        config.setAllowedOrigins(List.of("http://localhost:5173",
+        		"http://localhost", "http://frontend:5173"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
